2bb28977b251e530524ddaf8caaf86313d539b55
Tim-Fedor <31453165+Tim-Fedor@users.noreply.github.com>
2021-02-20 08:48:59 +0200

    Closes #58

--- test_list.py
+++ test_list.py
@@ -15,8 +15,8 @@ class TestListMethods(unittest.TestCase):
         self.assertEqual(2*[1, 3, 5], [1, 3, 5, 1, 3, 5])
         
     def test_tymothy_fedorchuk_fi_93(self):
-        self.assertEqual(max([1, 10, 100]),  10)
-        self.assertEqual(max([4, 12, 1]), 1)
+        self.assertEqual(max([1, 10, 100]),  100)
+        self.assertEqual(max([4, 12, 1]), 15)
         self.assertEqual(max([1, 2, 3]), 4)
 
 if __name__ == '__main__':
